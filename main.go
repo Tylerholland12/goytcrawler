0f3f39c7bdb881f010c1867e8acaca7a4ffac7a2
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-05-21 19:08:00 -0500

    [initial]

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"github.com/steelx/extractlinks "
+)
+
+func main() {
+	baseURL := "https://www.youtube.com/"
+
+	config := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	transport := &http.Transport{
+		TLSClientConfig: config,
+	}
+	netClient := &http.Client{
+		Transport: transport,
+	}
+
+	response, err := netClient.Get(baseURL)
+	defer response.Body.Close()
+
+	checkErr(err)
+
+	links, err := extractlinks.All(response.Body)
+	checkErr(err)
+
+	fmt.Println(links)
+
+	response.Body.Close()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
